2c24ad67436420a78070cce6093b5dbb10643dc7
yilativs <yilativs@gmail.com>
2016-02-27 00:57:56 +0300

    [add] reference vs copy vs serialization copy cache comparison

---
+++ src/main/java/CacheCopyStrategyComparison.java
@@ -0,0 +1,63 @@
+
+import java.util.Random;
+
+import foo.model.User;
+import foo.service.UserService;
+
+public class CacheCopyStrategyComparison {
+	private static final int DIFFERENT_ID_COUNT = 100;
+	private static final int REQUESTS_COUNT = 1000000;
+
+	public static void main(String[] args) {
+		UserService userService = new UserService();
+
+		Integer[] randomIdArray = getRandomIdArray(REQUESTS_COUNT, DIFFERENT_ID_COUNT);
+
+		
+		long startReferenceTime = System.currentTimeMillis();
+		for (Integer id : randomIdArray) {
+//			long interationStartTime = System.currentTimeMillis();
+			User user = userService.getUserFromReferenceCache(id);
+//			long timeTaken = System.currentTimeMillis() - interationStartTime;
+			// System.out.println("id=" + user.getId() + " time=" + timeTaken);
+		}
+		long totalReferenceTimeTaken = System.currentTimeMillis() - startReferenceTime;
+		System.out.println("totalReferenceTimeTaken=" + totalReferenceTimeTaken);
+
+		long startCopyTime = System.currentTimeMillis();
+		for (Integer id : randomIdArray) {
+//			long interationStartTime = System.currentTimeMillis();
+			User user = userService.getUserFromCopyBasedCache(id);
+//			long timeTaken = System.currentTimeMillis() - interationStartTime;
+			// System.out.println("id=" + user.getId() + " time=" + timeTaken);
+		}
+		double totalCopyTimeTaken = System.currentTimeMillis() - startCopyTime;
+
+		System.out.println("totalCopyTimeTaken=" + totalCopyTimeTaken);
+		
+		
+		
+		long startSerializedTime = System.currentTimeMillis();
+		for (Integer id : randomIdArray) {
+//			long interationStartTime = System.currentTimeMillis();
+			User user = userService.getUserFromSerializedCache(id);
+//			long timeTaken = System.currentTimeMillis() - interationStartTime;
+			// System.out.println("id=" + user.getId() + " time=" + timeTaken);
+		}
+		long totalSerializedTimeTaken = System.currentTimeMillis() - startSerializedTime;
+		System.out.println("totalSerializedTimeTaken=" + totalSerializedTimeTaken);
+
+
+		userService.close();
+	}
+
+	public static Integer[] getRandomIdArray(int size, int bound) {
+		Integer[] result = new Integer[size];
+		Random random = new Random();
+		for (int i = 0; i < result.length; i++) {
+			result[i] = random.nextInt(bound);
+		}
+		return result;
+	}
+
+}
--- src/main/java/foo/service/UserService.java
+++ src/main/java/foo/service/UserService.java
@@ -10,12 +10,12 @@ import net.sf.ehcache.Element;
 public class UserService implements Closeable {
 	private static final String USER_BY_ID_COPY_STRATEGY_BASED_CACHE = "userByIdCopyStrategyBasedCache";
 	private static final String USER_BY_ID_SERIALIZATION_BASED_CACHE = "userByIdSerializationBasedCache";
+	private static final String USER_BY_ID_REFERENCE_BASED_CACHE = "userByIdRefenceBasedCache";
 	private static final String USER_LOGIN_CACHE_NAME = "loginById";
 	// it returns a signleton instance or returns already created singleton instance (for multiple use newInstance)
 	private CacheManager cacheManager = CacheManager.create();
 
 	public String getCachedLogin(Integer id) {
-
 		Element element = cacheManager.getCache(USER_LOGIN_CACHE_NAME).get(id);
 		if (element == null) {
 			String login = getLogin(id);
@@ -26,16 +26,25 @@ public class UserService implements Closeable {
 	}
 
 	private String getLogin(int id) {
-		try {
+//		try {
 			// we will sleep id seconds :-)
-			Thread.sleep(3 * 1000);
-		} catch (InterruptedException e) {
-			Thread.interrupted();// who dared to interrupt me?
-		}
+//			Thread.sleep(3 * 1000);
+//		} catch (InterruptedException e) {
+//			Thread.interrupted();// who dared to interrupt me?
+//		}
 		// we want to store relativly large strings to check the speed of disk cache
-		char[] chars = new char[100000];
+		char[] chars = new char[1000];
 		Arrays.fill(chars, 'a');
 		return Integer.toString(id) + new String(chars);
+	}
+	
+	public User getUserFromReferenceCache(int id) {
+		Element element = cacheManager.getCache(USER_BY_ID_REFERENCE_BASED_CACHE).get(id);
+		if (element == null) {
+			element = new Element(id, new User(id, getLogin(id)));
+			cacheManager.getCache(USER_BY_ID_REFERENCE_BASED_CACHE).put(element);
+		}
+		return (User) element.getObjectValue();
 	}
 
 	public User getUserFromSerializedCache(int id) {
