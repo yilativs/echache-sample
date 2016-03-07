7f810c09914d00c1ce30eccfefc6c748c92fc135
yilativs <yilativs@gmail.com>
2016-03-07 18:25:58 +0300

    [add] cache exposed via JMX example added

--- src/main/java/foo/service/UserCacheAsideService.java
+++ src/main/java/foo/service/UserCacheAsideService.java
@@ -1,17 +1,28 @@
 package foo.service;
 
 import java.io.Closeable;
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
 
 import foo.model.User;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
+import net.sf.ehcache.management.ManagementService;
 
 public class UserCacheAsideService extends UserService implements Closeable {
 	public static final String USER_BY_ID_COPY_STRATEGY_BASED_CACHE = "userByIdCopyStrategyBasedCache";
 	public static final String USER_BY_ID_SERIALIZATION_BASED_CACHE = "userByIdSerializationBasedCache";
 	public static final String USER_BY_ID_REFERENCE_BASED_CACHE = "userByIdRefenceBasedCache";
 	// it returns a signleton instance or returns already created singleton instance (for multiple use newInstance)
-	private CacheManager cacheManager = CacheManager.create();
+
+	private final CacheManager cacheManager;
+
+	public UserCacheAsideService() {
+		 cacheManager = CacheManager.create();
+		 MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+		 ManagementService.registerMBeans(cacheManager, mBeanServer, false, false, false, true);
+	}
 
 	public User getUserFromReferenceCache(int id) {
 		return getUser(id, USER_BY_ID_REFERENCE_BASED_CACHE);
